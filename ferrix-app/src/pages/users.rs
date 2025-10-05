c5681b0579c2311d105f20583d289ed9fd8351c9
mskrasnov <mskrasnov07@ya.ru>
2025-10-05 13:05:55 +0300

    Implement 'users' page

---
+++ ferrix-app/src/pages/users.rs
@@ -0,0 +1,35 @@
+//! Users list page
+
+use crate::{
+    Message,
+    pages::{InfoRow, fmt_val, kv_info_table},
+};
+use ferrix_lib::sys::Users;
+
+use iced::widget::{center, column, container, scrollable, text};
+
+pub fn users_page<'a>(users: &'a Option<Users>) -> container::Container<'a, Message> {
+    match users {
+        None => container(center(text("Загрузка данных..."))),
+        Some(users) => {
+            let mut users_list = column![].spacing(5);
+            for usr in &users.users {
+                let rows = vec![
+                    InfoRow::new("Имя пользователя", Some(usr.name.clone())),
+                    InfoRow::new("ID пользователя", fmt_val(Some(usr.uid))),
+                    InfoRow::new("ID группы", fmt_val(Some(usr.gid))),
+                    InfoRow::new("GECOS", usr.gecos.clone()),
+                    InfoRow::new("Домашний каталог", Some(usr.home_dir.clone())),
+                    InfoRow::new("Оболочка входа", Some(usr.login_shell.clone())),
+                ];
+                let usr_view = column![
+                    text(format!("Пользователь #{}", usr.gid)).style(text::warning),
+                    container(kv_info_table(rows)).style(container::rounded_box),
+                ]
+                .spacing(5);
+                users_list = users_list.push(usr_view);
+            }
+            container(scrollable(users_list))
+        }
+    }
+}
