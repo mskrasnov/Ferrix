4a6a79b6e9744d778cbe507ef566599a31b1d455
mskrasnov <mskrasnov07@ya.ru>
2025-10-28 15:29:39 +0300

    Add license notice

--- ferrix-app/src/styles.rs
+++ ferrix-app/src/styles.rs
@@ -1,3 +1,23 @@
+/* styles.rs
+ *
+ * Copyright 2025 Michail Krasnov <mskrasnov07@ya.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 use iced::{Theme, widget::button};
 
 /// A link button
